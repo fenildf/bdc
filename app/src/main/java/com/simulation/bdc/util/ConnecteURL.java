3f46c9d9467e74b79aae6b87c4fea1d766930182
rosycloudss <37603327+rosycloudss@users.noreply.github.com>
2018-05-23 12:43:43 +0800

    添加注释

---
+++ app/src/main/java/com/simulation/bdc/util/ConnecteURL.java
@@ -0,0 +1,34 @@
+package com.simulation.bdc.util;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class ConnecteURL {
+
+    private static final String TAG = "ConnecteURL";
+
+    /**
+     * 访问后台服务单，并返回服务端传过来的json数据
+     *
+     * @param url
+     * @return
+     */
+    public String getJosnString(String url) {
+        String responseData = null;
+        if (url != null) {
+            try {
+                OkHttpClient client = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS).build();
+                Request request = new Request.Builder().url(url).build();
+                Response response = client.newCall(request).execute();
+                responseData = response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return responseData;
+    }
+}
---
+++ app/src/main/java/com/simulation/bdc/util/Session.java
@@ -0,0 +1,39 @@
+package com.simulation.bdc.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Session {
+
+    private static Map<String, Object> map = new HashMap<String, Object>();
+
+    /**
+     * 获取session中缓存的对象
+     *
+     * @param key
+     * @return
+     */
+    public static Object getAttribute(String key) {
+        return map.get(key);
+    }
+
+    /**
+     * 移除在session中缓存的对象
+     *
+     * @param key
+     */
+    public static void remove(String key) {
+        map.remove(key);
+    }
+
+    /**
+     * 向session中添加对象
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static Object put(String key, Object value) {
+        return map.put(key, value);
+    }
+}
