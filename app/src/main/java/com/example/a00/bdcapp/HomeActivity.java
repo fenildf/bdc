213526f92e068b3f11a6380a5f179829974fc8bd
lw <1>
2018-05-27 20:11:50 +0800

    homeactivity修改

--- app/src/main/java/com/example/a00/bdcapp/HomeActivity.java
+++ app/src/main/java/com/example/a00/bdcapp/HomeActivity.java
@@ -1,80 +1,99 @@
 package com.example.a00.bdcapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.a00.fragment1.MyCoursesActivity;
+import com.example.a00.fragment1.ReviewPlanActivity;
+import com.example.a00.fragment1.StoryReadingActivity;
+import com.example.a00.fragment1.WordInfoActivity;
+
+
+/**
+ * 首页，通过首页的泡泡，点击添加事件，分别跳转到各个对应功能的子页面
+ */
 public class HomeActivity extends AppCompatActivity {
-    private ViewPager viewPager;
-    private MenuItem menuItem;
-    private BottomNavigationView navigation;
+    private EditText print;
+    private Button search;
+    private Button myCourse;
+    private Button review;
+    private Button mine;
+    private Button story;
+    private TextView todayTask;
+    private TextView wordNumber;
+    private Button startReciteWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
-
-
-/*
-        viewPager = findViewById(R.id.viewpager);
-        navigation = findViewById(R.id.navigation);
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        print = findViewById(R.id.print);
+        search = findViewById(R.id.search);
+        myCourse = findViewById(R.id.mycourse);
+        review = findViewById(R.id.review);
+        mine = findViewById(R.id.mine);
+        story = findViewById(R.id.story);
+        todayTask = findViewById(R.id.today_task);
+        wordNumber = findViewById(R.id.word_number);
+        startReciteWord = findViewById(R.id.start_recite_word);
+        //点击查询，跳转到单词释义显示界面
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                if (menuItem != null) {
-                    menuItem.setChecked(false);
-                } else {
-                    navigation.getMenu().getItem(0).setChecked(false);
-                }
-                menuItem = navigation.getMenu().getItem(position);
-                menuItem.setChecked(true);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, WordInfoActivity.class);
+                startActivity(intent);
             }
         });
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        setupViewPager(viewPager);*/
-    }
-
-/*    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    viewPager.setCurrentItem(0);
-                    return true;
-                case R.id.navigation_dashboard:
-                    viewPager.setCurrentItem(1);
-                    return true;
-                case R.id.navigation_notifications:
-                    viewPager.setCurrentItem(2);
-                    return true;
+        //点击我的课程，跳转到我的课程页面，显示已收藏教材和已下载的课程
+        myCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MyCoursesActivity.class);
+                startActivity(intent);
             }
-            return false;
-        }
-    };*/
+        });
+        //点击复习，跳转到复习界面，显示要复习的内容
+        review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, ReviewPlanActivity.class);
+                startActivity(intent);
+            }
+        });
+        //点击我的，跳到用户管理界面，显示用户头像，生词本等内容
+        mine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, BlankFragmentThree.class);
+                startActivity(intent);
+            }
+        });
+        //点击故事，跳转到故事阅读，显示小故事
+        story.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, StoryReadingActivity.class);
+                startActivity(intent);
+            }
+        });
+        //点击开始背单词，跳转到背诵单词的界面
+        startReciteWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,StartReciteWordActivity.class);
+                startActivity(intent);
+            }
+        });
 
-/*    //用来把三个底部导航栏指引的界面添加到点击的事件中，比如点击了BlankFragmentOne就是首页，Two就是课程
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new BlankFragmentOne());
-        adapter.addFragment(new BlankFragmentTwo());
-        adapter.addFragment(new BlankFragmentThree());
-        viewPager.setAdapter(adapter);
-    }*/
+    }
 }
