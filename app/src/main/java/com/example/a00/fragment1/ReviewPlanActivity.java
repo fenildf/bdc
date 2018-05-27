c201b33515f6d72bc6a64e65e5ab4168b9538812
lw <1>
2018-05-27 21:44:02 +0800

    ReviewPlanActivity 点击首页复习

--- app/src/main/java/com/example/a00/fragment1/ReviewPlanActivity.java
+++ app/src/main/java/com/example/a00/fragment1/ReviewPlanActivity.java
@@ -6,18 +6,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.a00.bdcapp.R;
 
 /**
- * 这个是位于我的课程下面的复习计划
+ * 点击首页的泡泡中的复习
  */
 public class ReviewPlanActivity extends AppCompatActivity {
+
+    private TextView wordComplement;//单词补全
+    private ImageButton pronounce;//单词发音
+    private EditText wordWrite;//写出这个单词
+    private TextView wordRemind;//单词提示
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_plan);
+
+        wordComplement = findViewById(R.id.word_complement);
+        pronounce = findViewById(R.id.pronouce);
+        wordWrite = findViewById(R.id.word_write);
+        wordRemind = findViewById(R.id.word_remind);
+
     }
 
 }
